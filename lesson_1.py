f81eb30c1cd0f130e0d852467499996b44471e4f
Andrii <andreygold1@gmail.com>
2022-09-06 09:36:42 +0300

    Home work lesson 4

--- lesson_1.py
+++ lesson_1.py
@@ -1,1 +1,7 @@
 print("Hello Python Basic 25_08_2022")
+
+value = "123456789"
+
+my_str = value if len(value) < 5 else value[1::2]
+
+print(my_str[::-1])
---
+++ lesson_2.py
@@ -0,0 +1,16 @@
+# PEP-8 formatting Alt+CTRL+L
+# print(2+3)
+
+value_1 = "2"
+value_2 = "4"
+
+# value_2 = 5
+# result = value_1 * value_2
+# result = value_1 / value_2
+# result = value_1 // value_2
+# result = value_1 % value_2
+# result = value_1 ** value_2
+# print(value_1)
+
+# value = ("1"+"2")*3
+print(value_1 + value_2)
