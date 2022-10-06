60f3d845ac570e39665330cf62cbe3954b01978b
Andrii <andreygold1@gmail.com>
2022-10-06 22:43:54 +0300

    coursework

--- coursework/trader.py
+++ coursework/trader.py
@@ -1,13 +1,3 @@
-# //Аргументы запуска файла:
-# //    RATE - получение текущего курса (USD/UAH)
-# //    AVAILABLE - получение остатков по счетам
-# //    BUY XXX - покупка xxx долларов. При отсутсвии грвен для покупки выводит сообщение типа UNAVAILABLE, REQUIRED BALANCE UAH 2593.00, AVAILABLE 1000.00
-# //    SELL XXX - продажа xxx долларов. При отсутсвии долларов для продажи выводит сообщение типа UNAVAILABLE, REQUIRED BALANCE USD 200.00, AVAILABLE 135.00
-# //    BUY ALL - покупка долларов на все возможные гривны.
-# //    SELL ALL - продажа всех долларов.
-# //    NEXT - получить следующий курс
-# //    RESTART - начать игру с начала (с начальными условиями)
-
 from argparse import ArgumentParser
 
 args = ArgumentParser()
@@ -17,7 +7,6 @@ args.add_argument("action", type=str)
 # args.add_argument("--job", type=str, nargs='?', default='')
 
 args = vars(args.parse_args())
-print(args)
 
-# if args['action'] == "NEXT":
-#     print(args['action'])
+print(args)
+action_in = choice_action(args)
---
+++ coursework/utils.py
@@ -0,0 +1,3 @@
+def choice_action(args):
+    if args['action'] == "NEXT":
+        print(args['action'])
