b99fc304604f01b3aa10cd6757a0d3ef1009ced5
Andrii <andreygold1@gmail.com>
2022-10-14 00:47:24 +0300

    Courswork

--- coursework/utils.py
+++ coursework/utils.py
@@ -1,4 +1,5 @@
 import json
+import math
 import random
 
 
@@ -13,65 +14,102 @@ def read_json(filename):
     return data
 
 
-def write_json(filename1, filename2):
+def write_json(filename1, data):
     with open(filename1, 'w') as json_file:
-        json.dump(filename2, json_file, indent=2)
+        json.dump(data, json_file, indent=2)
 
+
+def change_cource(data):
+    next_course = round(random.uniform((data["course"] - data['delta']),
+                                       (data["course"] + data['delta'])), 2)
+    data["course"] = next_course
+    return data["course"]
+
+
+def buy_all(data):
+    summa_usd = make_rounding(data["UAH"] / data['course'])
+    if summa_usd > 0:
+        data['USD'] = data['USD'] + summa_usd
+        data['UAH'] = round(data['UAH'] - data['USD'] * data['course'], 2)
+        return data
+    else:
+        print('Недостаточно средств для покупки валюты')
+
+
+def sell_all(data):
+    summa_uah = data['USD'] * data['course']
+    print(summa_uah)
+    data['UAH'] = data['UAH'] + round(summa_uah, 2)
+    data['USD'] = data['USD'] - data['USD']
+
+
+def buy_parts(data, value):
+    try:
+        value_2_float = round(float(value), 2)
+        need_uah = round(value_2_float * data['course'],2)
+        if data['UAH'] - need_uah < 0:
+            print('UNAVAILABLE, REQUIRED BALANCE UAH ' + str(need_uah) + ', ' + 'AVAILABLE ' + str(
+                data['UAH']))
+        else:
+            data['UAH'] = round(data['UAH'] - need_uah, 2)
+            data["USD"] = data["USD"] + value_2_float
+        return data
+    except ValueError:
+        print("Ошибка ввода суммы покупки")
+
+
+def sell_parts(data, value):
+    try:
+        value_2_float = round(float(value), 2)
+        if data['USD'] - value_2_float < 0:
+            print('UNAVAILABLE, REQUIRED BALANCE USD ' + str(value_2_float) + ', ' + 'AVAILABLE ' + str(
+                data['USD']))
+        else:
+            data['USD'] = round(data['USD'] - value_2_float, 2)
+            data["UAH"] = data["UAH"] + round(value_2_float * data['course'], 2)
+    except ValueError:
+        print("Ошибка ввода суммы продажи")
+
+def make_rounding(value):
+    print(value)
+    temp_value = math.floor(value * 100)
+    print(temp_value)
+    print(temp_value / 100)
+    return temp_value / 100
+
+def read_txt(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            print(line.strip())
 
 def choice_action(args):
+    info_system = read_json(filename='state_system.json')
     if args['value_1'] == 'NEXT' and args['value_2'] == '':
-        info_system = read_json(filename='state_system.json')
-        next_course = round(random.uniform((info_system["course"] - info_system['delta']),
-                                           (info_system["course"] + info_system['delta'])), 2)
-        info_system["course"] = next_course
-        write_json(filename1='state_system.json', filename2=info_system)
+        change_cource(info_system)
+        write_json(filename1='state_system.json', data=info_system)
     elif args['value_1'] == 'RATE' and args['value_2'] == '':
-        info_system = read_json(filename='state_system.json')
-        info_course = info_system["course"]
-        print(info_course)
+        print(info_system["course"])
     elif args['value_1'] == 'AVAILABLE' and args['value_2'] == '':
-        info_system = read_json(filename='state_system.json')
         print("USD", info_system["USD"], "UAH", info_system["UAH"])
     elif args['value_1'] == 'BUY' and args['value_2'] != 'ALL':
-        try:
-            value_2_float = float(args['value_2'])
-            info_system = read_json(filename='state_system.json')
-            need_uah = value_2_float * info_system['course']
-            if info_system['UAH'] - need_uah < 0:
-                print('UNAVAILABLE, REQUIRED BALANCE UAH ' + str(need_uah) + ', ' + 'AVAILABLE ' + str(
-                    info_system['UAH']))
-            else:
-                info_system['UAH'] = info_system['UAH'] - need_uah
-                info_system["USD"] = info_system["USD"] + value_2_float
-                write_json(filename1='state_system.json', filename2=info_system)
-        except ValueError:
-            pass
+        buy_parts(info_system, args['value_2'])
+        write_json(filename1='state_system.json', data=info_system)
     elif args['value_1'] == 'BUY' and args['value_2'] == 'ALL':
-        info_system = read_json(filename='state_system.json')
-        summa_usd = info_system["UAH"] / info_system['course']
-        info_system['USD'] = round(info_system['USD'] + summa_usd, 2)
-        info_system['UAH'] = info_system['UAH'] - info_system['UAH']
-        write_json(filename1='state_system.json', filename2=info_system)
+        buy_all(info_system)
+        write_json(filename1='state_system.json', data=info_system)
     elif args['value_1'] == 'SELL' and args['value_2'] == 'ALL':
-        info_system = read_json(filename='state_system.json')
-        summa_uah = round(info_system['USD'] * info_system['course'], 2)
-        info_system['UAH'] = info_system['UAH'] + summa_uah
-        info_system['USD'] = info_system['USD'] - info_system['USD']
-        write_json(filename1='state_system.json', filename2=info_system)
+        sell_all(info_system)
+        write_json(filename1='state_system.json', data=info_system)
     elif args['value_1'] == 'SELL' and args['value_2'] != 'ALL':
-        try:
-            value_2_float = float(args['value_2'])
-            info_system = read_json(filename='state_system.json')
-            if info_system['USD'] - value_2_float < 0:
-                print('UNAVAILABLE, REQUIRED BALANCE USD ' + str(value_2_float) + ', ' + 'AVAILABLE ' + str(
-                    info_system['USD']))
-            else:
-                info_system['USD'] = round(info_system['USD'] - value_2_float, 2)
-                info_system["UAH"] = round(info_system["UAH"] + value_2_float * info_system['course'], 2)
-            write_json(filename1='state_system.json', filename2=info_system)
-        except ValueError:
-            pass
+        sell_parts(info_system, args['value_2'])
+        write_json(filename1='state_system.json', data=info_system)
     elif args['value_1'] == 'RESTART' and args['value_2'] == '':
         make_restart()
     else:
-        pass
+        print("Недопустимая команда! Укажите одну из команд\n")
+        read_txt('action_guide.txt')
+
+
